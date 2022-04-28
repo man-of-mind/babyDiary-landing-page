ed588fdee6097c6838756bdac2e4dd47e63fb670
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-04-28 07:01:04 +0100

    done

--- src/Components/HomePage/home.tsx
+++ src/Components/HomePage/home.tsx
@@ -28,6 +28,12 @@ import appStore from "../../images/appStore.svg";
 import diaryImage from "../../images/diary-image.svg";
 import googlePlay from "../../images/googlePlay.svg";
 import blogImage from "../../images/blog-image.svg";
+import logo from "../../images/logo.svg";
+import babyDiaryLogo from "../../images/babyDiaryColored.svg";
+import facebook from "../../images/facebook.svg";
+import instagram from "../../images/instagram.svg";
+import twitter from "../../images/twitter.svg";
+import Mlogo from "../../images/m-logo.svg";
 
 
 export default function Home() {
@@ -246,6 +252,16 @@ export default function Home() {
                             Consult with a medical professional if you have health concerns. 
                             Use of this site is subject to our <a href="localhost:3000/">Terms of Use</a> and <a href="localhost:3000/">Privacy Policy.</a>
                         </p>
+                    </div>
+                    <hr></hr>
+                    <div className={styles['footer-icons']}>
+                        <img src={logo} alt="logo"></img>
+                        <img src={babyDiaryLogo} alt="baby videos" className={styles['logo']} />
+                        <p>&#169; Baby Diary. 2020</p>
+                        <img src={facebook} alt="facebook" className={styles['facebook']}></img>
+                        <img src={twitter} alt="twitter"></img>
+                        <img src={instagram} alt="instagram" className={styles['instagram']}></img>
+                        <img src={Mlogo} alt="mLogo app"></img>
                     </div>
                 </footer>
             </div>
